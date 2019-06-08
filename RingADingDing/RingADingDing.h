d18c3dcab5abc6ec6ca7b4a562e9e7cef41fe194
Dean Thibault <deanthibault@digitalbeans.net>
2019-06-08 15:27:14 -0400

    Initial Commit

---
+++ RingADingDing/RingADingDing.h
@@ -0,0 +1,19 @@
+//
+//  RingADingDing.h
+//  RingADingDing
+//
+//  Created by Dean Thibault on 6/8/19.
+//  Copyright © 2019 Digital Beans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RingADingDing.
+FOUNDATION_EXPORT double RingADingDingVersionNumber;
+
+//! Project version string for RingADingDing.
+FOUNDATION_EXPORT const unsigned char RingADingDingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RingADingDing/PublicHeader.h>
+
+
